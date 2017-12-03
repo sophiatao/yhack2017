ce0038d68b645eb78661368f08b08533efd137f4
inksplatt <30760803+sophiatao@users.noreply.github.com>
2017-12-03 03:49:04 -0500

    It works

--- js/animation.js
+++ js/animation.js
@@ -3,6 +3,8 @@ var info = {
     lastname: null,
     email: null,
     city: null,
+    longitude: null,
+    latitude: null,
     address: null,
     state: null,
     maritalstatus: null,
@@ -70,6 +72,15 @@ function getNextForm(param) {
                     alert("Invalid address");
                     return;
                 }
+                $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?address="+info.city, function(val) {
+                    if(val.results.length) {
+                        console.log("query success");
+                      var location = val.results[0].geometry.location;
+                      info.latitude = location.lat;
+                      info.longitude = location.lng;
+                    }
+                    else console.log("query failed");
+                })
                 info.state = document.getElementById("state").value; //selecting
                 if (!info.state) {
                     alert("Invalid state");
@@ -127,6 +138,8 @@ function submit() {
         console.log(xhr.status);
         console.log(xhr.responseText);
     }
+    console.log(xhr.status);
+    console.log(xhr.responseText);
 
 }
 
@@ -148,25 +161,15 @@ function addForm() {
         return;
     }
     mednum++;
-    var e = document.getElementById("medical-form");
-    var newdiv = document.createElement("input");
-     newdiv.name="q";
-    newdiv.type="text";
-    newdiv.id="med"+mednum;
-    newdiv.placeholder="Enter a condition here";
-    newdiv.style="color:#fff;width:75%;max-width:600px;outline:0";
-    e.insertBefore(newdiv, document.getElementById("addform"));
-    var newdiv2 = document.createElement("input");
-    newdiv2.list="riskfactor";
-    newdiv2.style="width:3em";
-    newdiv2.id="risk"+mednum;
-    e.insertBefore(newdiv2, document.getElementById("addform"));
+    // var e = document.getElementById("medical-form");
+    $("#med0").clone().prop({ id: "med"+ mednum }).appendTo("#med-container");
+    $("#risk0").clone().prop({ id: "risk"+ mednum }).appendTo("#med-container");
     var demo = new autoComplete({
             selector: '#med'+mednum,
             minChars: 1,
             source: function(term, suggest){
                 term = term.toLowerCase();
-                var choices = ['ActionScript', 'AppleScript', 'Asp', 'Assembly', 'BASIC', 'Batch', 'C', 'C++', 'CSS', 'Clojure', 'COBOL', 'ColdFusion', 'Erlang', 'Fortran', 'Groovy', 'Haskell', 'HTML', 'Java', 'JavaScript', 'Lisp', 'Perl', 'PHP', 'PowerShell', 'Python', 'Ruby', 'Scala', 'Scheme', 'SQL', 'TeX', 'XML'];
+                var choices = ["A15-A19 Tuberculosis","B20-B20 Human immunodeficiency virus [HIV] disease","D50-D53 Nutritional anemias","E08-E13 Diabetes mellitus","E65-E68 Overweight, obesity and other hyperalimentation","E73 Lactose intolerance","E84 Cystic fibrosis","F20-F29 Schizophrenia, schizotypal, delusional, and other non-mood psychotic disorders","F70-F79 Intellectual disabilities","G10 Huntington's disease","G11 Hereditary ataxia","G20 Parkinson's disease","G30 Alzheimer's disease","G35 Multiple sclerosis","G40 Epilepsy and recurrent seizures","G43 Migraine","G47 Sleep disorders","G80 Cerebral palsy","G91 Hydrocephalus","G92 Toxic encephalopathy","H40-H42 Glaucoma","H80 Otosclerosis","I10-I16 Hypertensive diseases","I20-I25 Ischemic heart diseases","I26 Pulmonary embolism","I30 Acute pericarditis","I40 Acute myocarditis","I46 Cardiac arrest","I50 Heart failure","I63 Cerebral infarction","I95 Hypotension","J09-J18 Influenza and pneumonia","J81 Pulmonary edema","K35 Acute appendicitis","K40-K46 Hernia","K50 Crohn's disease [regional enteritis]","K51 Ulcerative colitis","K65 Peritonitis","K70 Alcoholic liver disease","K74 Fibrosis and cirrhosis of liver","K85 Acute pancreatitis","L20 Atopic dermatitis","L21 Seborrheic dermatitis","L22 Diaper dermatitis","L23 Allergic contact dermatitis","L24 Irritant contact dermatitis","L40 Psoriasis","L41 Parapsoriasis","L60 Nail disorders","L70 Acne","L71 Rosacea","L80 Vitiligo","L89 Pressure ulcer","M08 Juvenile arthritis","M10 Gout","M1A Chronic gout","M15-M19 Osteoarthritis","N17 Acute kidney failure","N18 Chronic kidney disease (CKD)","N46 Male infertility","N52 Male erectile dysfunction","N91 Absent, scanty and rare menstruation","N92 Excessive, frequent and irregular menstruation","N96 Recurrent pregnancy loss","N97 Female infertility","Q02 Microcephaly","Q05 Spina bifida","Q35-Q37 Cleft lip and/or cleft palate","Q61 Cystic kidney disease","Q90 Down syndrome","Q91 Trisomy 18 and Trisomy 13","Q96 Turner's syndrome","R17 Unspecified jaundice","S00-S09 Injuries to the head","S10-S19 Injuries to the neck","T33-T34 Frostbite","Z66-Z66 Do not resuscitate status","Z68-Z68 Body mass index (BMI)"];
                 var suggestions = [];
                 for (i=0;i<choices.length;i++)
                     if (~choices[i].toLowerCase().indexOf(term)) suggestions.push(choices[i]);
